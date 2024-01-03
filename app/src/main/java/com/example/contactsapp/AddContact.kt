cd8a142a68567c3735bf7f61379a164fcca79794
nourhan36 <nourhanibrahim2004@gmail.com>
2024-01-04 00:18:01 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/contactsapp/AddContact.kt
@@ -0,0 +1,14 @@
+package com.example.contactsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddContact : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_contact)
+
+        // You can perform additional setup for AnotherActivity if needed
+    }
+}
--- app/src/main/java/com/example/contactsapp/MainActivity.kt
+++ app/src/main/java/com/example/contactsapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.contactsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn: FloatingActionButton = findViewById(R.id.btn_add)
+
+        // Set an OnClickListener to handle the click event
+        btn.setOnClickListener {
+            // Open another activity when the button is clicked
+            val intent = Intent(this@MainActivity, AddContact::class.java)
+            startActivity(intent)
+        }
     }
 }
